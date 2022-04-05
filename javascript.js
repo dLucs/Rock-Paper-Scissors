750e49ed0132f4047f4179a2bfea5d0aaae493f2
Lucas <lucas_61@hotmail.es>
2022-04-05 15:21:07 +0200

    Created the main function to run the game

--- javascript.js
+++ javascript.js
@@ -1,5 +1,40 @@
-const selection = ['Rock', 'Paper', 'Scissors']
-function computerPlay(){
+const selection = ['rock', 'paper', 'scissors'];
+   
+
+
+function playRound(){
+    
+    let playerSelection = prompt('Please choose Rock, Paper, or Scissors: ').toLowerCase();
+    // if (selection.includes(playerSelection))
+    
     let computerSelection = selection[Math.floor(Math.random() * selection.length)];
-    return computerSelection;
+
+//Rock Choice
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Loser! Paper beats Rock";
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        return "It's a Draw";
+    }
+//paper Choice
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "It's a Draw";
+    }
+//scissors Choice
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beat Paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "It's a Draw";
+    } else {
+        return "Something Went Wrong";
+    }
+
 }
+console.log(playRound())
