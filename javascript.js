30412eee16994122139b3bc792563d8c673e2dc4
Lucas <lucas_61@hotmail.es>
2022-04-22 15:19:56 +0200

    refactored original JS code to work with new parameters

--- javascript.js
+++ javascript.js
@@ -4,13 +4,24 @@ const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
 const resetButton = document.querySelector('#reset');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const winner = document.querySelector('#winner')
 
 rockButton.addEventListener('click', () => playRound())
-    playerSelection = 'rock'
+    playerSelection = 'rock';
+
+paperButton.addEventListener('click', () => playRound())
+    playerSelection = 'paper';
+
+scissorsButton.addEventListener('click', () => playRound())
+    playerSelection = 'scissors';
 
    
-let playerCounter = 0;
-let computerCounter = 0;
+let playerScore = 0;
+let computerScore = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 function playRound(){
 
@@ -19,53 +30,98 @@ function playRound(){
     
 //Rock Choice
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerCounter++
-        return "You Win! Rock beats Scissors!";
+        if (!isGameOver) {
+            playerScore++;
+            if (playerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Win!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+        
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        computerCounter++
-        return "Loser! Paper beats Rock";
+        if (!isGameOver) {
+            computerScore++;
+            if (computerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Lose!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+      
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        return "It's a Draw";
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+        
     }
 //paper Choice
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        playerCounter++
-        return "You Win! Paper beats Rock!";
+        if (!isGameOver) {
+            playerScore++;
+            if (playerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Win!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+   
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        computerCounter++
-        return "You Lose! Scissors beats Paper";
+        if (!isGameOver) {
+            computerScore++;
+            if (computerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Lose!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+     
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "It's a Draw";
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+    
     }
 //scissors Choice
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerCounter++
-        return "You win! Scissors beat Paper!";
+        if (!isGameOver) {
+            playerScore++;
+            if (playerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Win!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+       
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        computerCounter++
-        return "You Lose! Rock beats Scissors";
+        if (!isGameOver) {
+            computerScore++;
+            if (computerScore === winningScore) {
+                isGameOver = true;
+                winner.textContent = "You Lose!";
+            }
+        }
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+        
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a Draw";
+        player.textContent = playerScore;
+        computer.textContent = computerScore;
+    
     } else {
         return "Something Went Wrong";
     }
 
 }
+resetButton.addEventListener('click', function(){
+    isGameOver = false;
+    playerScore = 0;
+    computerScore = 0;
+    player.textContent = 0;
+    computer.textContent = 0;
 
-// function game(){
-//     for (let i = 0; i < 5; i++){
-//     console.log(playRound())
-//     }
-//     if (playerCounter > computerCounter){
-//         console.log (playerCounter, ":", computerCounter, "You Win!")
-//     }
-//     else if (playerCounter < computerCounter){
-//         console.log (playerCounter, ":", computerCounter, "You Lose!")
-    
-//     }
-//     else {
-//         console.log(playerCounter, ":", computerCounter, "It's a Draw")
-//     }
-
-// }
+})
 
